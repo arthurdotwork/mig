4b3b77a5ffed9c02d2cf32a45083595d76585e76
arthurdotwork <35616365+arthurdotwork@users.noreply.github.com>
2025-01-20 21:24:09 +0100

    feat: init commands structure

---
+++ cmd/mig/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/arthurdotwork/mig/cmd/mig/commands"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sig
+		cancel()
+	}()
+
+	rootCmd := &cobra.Command{
+		Use:   "mig",
+		Short: "mig is a tool for managing database migrations",
+	}
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "migrate",
+		Short: "run the migrations",
+		RunE:  commands.Migrate(ctx),
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
